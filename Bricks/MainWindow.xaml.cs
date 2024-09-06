70e6d2d96f2677b1a5fd0e59a1e2efa6757e3f38
Joj007 <sinkajozsef2005@gmail.com>
2024-09-06 10:28:47 +0200

    Dg feltöltés

--- Bricks/MainWindow.xaml.cs
+++ Bricks/MainWindow.xaml.cs
@@ -9,20 +9,39 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
+using System.Collections.ObjectModel;
 
 namespace Bricks
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Termek> termekek = new ObservableCollection<Termek>();
         public MainWindow()
         {
             InitializeComponent();
+            dgTermekLista.ItemsSource = termekek;
+            List<string> sorok = File.ReadAllLines("brickstore_parts_7288-1-mobile-police-unit.bsx").ToList();
 
-            string[] sorok = File.ReadAllLines("brickstore_parts_7288-1-mobile-police-unit.bsx");
-            MessageBox.Show(sorok[0]);
+            for (int i = 0; i < sorok.Count; i++)
+            {
+                if (sorok[i].Trim()=="<Item>")
+                {
+                    List<string> termekAdatok = new();
+                    for (int j = 1; j < 13; j++)
+                    {
+                        int tol = sorok[i + j].IndexOf(">");
+                        int ig = sorok[i + j].LastIndexOf("<");
+                        int length = sorok[i + j].Length;
+
+                        termekAdatok.Add(sorok[i + j].Substring(tol+1, ig-tol-1));
+                        
+                    }
+                    termekek.Add(new Termek(termekAdatok.ToArray()));
+
+                }
+            }
+
+
         }
     }
 }
---
+++ Bricks/Termek.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bricks
+{
+    class Termek
+    {
+        string id;
+        string typeId;
+        int colorId;
+        string name;
+        string typeName;
+        string colorName;
+        int categoryId;
+        string categoryName;
+        string status;
+        int quantity;
+        double price;
+        string condition;
+
+        public Termek(string id, string typeId, int colorId, string name, string typeName, string colorName, int categoryId, string categoryName, string status, int quantity, double price, string condition)
+        {
+            this.id = id;
+            this.typeId = typeId;
+            this.colorId = colorId;
+            this.name = name;
+            this.typeName = typeName;
+            this.colorName = colorName;
+            this.categoryId = categoryId;
+            this.categoryName = categoryName;
+            this.status = status;
+            this.quantity = quantity;
+            this.price = price;
+            this.condition = condition;
+        }
+
+        public Termek(string[] aru)
+        {
+            this.id = aru[0];
+            this.typeId = aru[1];
+            this.colorId = int.Parse(aru[2]);
+            this.name = aru[3];
+            this.typeName = aru[4];
+            this.colorName = aru[5];
+            this.categoryId = int.Parse(aru[6]);
+            this.categoryName = aru[7];
+            this.status = aru[8];
+            this.quantity = int.Parse(aru[9]);
+            this.price = double.Parse(aru[10].Replace('.', ','));
+            this.condition = aru[11];
+        }
+
+        public string Id { get => id;}
+        public string TypeId { get => typeId;}
+        public int ColorId { get => colorId;}
+        public string Name { get => name;}
+        public string TypeName { get => typeName;}
+        public string ColorName { get => colorName;}
+        public int CategoryId { get => categoryId;}
+        public string CategoryName { get => categoryName;}
+        public string Status { get => status;}
+        public int Quantity { get => quantity;}
+        public double Price { get => price;}
+        public string Condition { get => condition;}
+
+
+    }
+}
